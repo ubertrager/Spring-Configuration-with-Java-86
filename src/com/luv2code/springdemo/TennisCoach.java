780dd6ceb51ec0a1d252fabe48591f2a2dc43684
Gero Viertel <gero.viertel@hotmail.de>
2019-11-13 22:29:04 +0100

    changed setter injection to method injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -13,10 +13,9 @@ public class TennisCoach implements Coach {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
 	
-	// define a setter method
 	@Autowired
-	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println(">> TennisCoach: inside setFortuneService method");
+	public void foobar(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach: inside foobar method");
 		this.fortuneService = fortuneService;
 	}
 		

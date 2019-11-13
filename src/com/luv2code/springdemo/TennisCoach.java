0fda110d5dda008091bb08e34b406ae1e46b58b2
Gero Viertel <gero.viertel@hotmail.de>
2019-11-13 22:25:25 +0100

    changed tennis coach to setter injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -8,11 +8,17 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
-	@Autowired
-	public TennisCoach(FortuneService fortuneService) {
-		this.fortuneService = fortuneService;
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
 	}
 	
+	// define a setter method
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+		
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";

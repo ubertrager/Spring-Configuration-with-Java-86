e88c5fba8f9a478b2e3838aac54f400217cbb917
Gero Viertel <gero.viertel@hotmail.de>
2019-11-13 21:50:38 +0100

    basic demo for bean initialization using class path scanning and annotations

---
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// get the bean from spring contain
+		Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+		
+		// call a method on the bean	
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// close the context
+		context.close();
+	}
+
+}

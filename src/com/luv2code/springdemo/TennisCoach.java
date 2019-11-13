1803d5280e86cefda5038fc3141d281bf4530ef3
Gero Viertel <gero.viertel@hotmail.de>
2019-11-13 22:34:42 +0100

    changed method injection to field injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,19 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
-	
-	@Autowired
-	public void foobar(FortuneService fortuneService) {
-		System.out.println(">> TennisCoach: inside foobar method");
-		this.fortuneService = fortuneService;
-	}
-		
+			
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
